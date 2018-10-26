955517cd840f59d306818a3998051b46f136a153
Patrick <ctrlaltpat@gmail.com>
2018-10-26 08:54:47 +0100

    more anxiety

--- app/models/function.rb
+++ app/models/function.rb
@@ -4,7 +4,7 @@ class Function < ApplicationRecord
   belongs_to :location
 
   validates :topic, presence: true
-  validates :capacity, presence: true
+  validates :capacity, presence: true, numericality: { only_integer: true, :less_than_or_equal_to => 10, :greater_than_or_equal_to => 0 }
 
   def users_as_params
     self.users.map {|user| "#{user.username}" }.join(", ")
